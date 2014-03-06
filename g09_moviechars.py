a80249fa32be62a205cb38a4bb39405463be72c7
Pratyaksh <pratyaksh@me.com>
2014-03-06 11:30:46 +0530

    Used unix wordlist to filter common words

--- g09_moviechars.py
+++ g09_moviechars.py
@@ -6,17 +6,32 @@ text = f.read()
 words = set(text.split())
 caps = set()
 
-punctuation = [',', '.', '\'', '\"', '!', '?', '...', ':']
+punctuation = [',', '.', '\'', '\"', '!', '?', '...', ':', ')', '(', '[', ']', '&']
 
 for word in words:
 	if (word[0].isupper()): 
-		if (word[-3:] in punctuation):
+		if (word[-3:] in punctuation):  # words ending with ...
 			word = word[:-3]
-		if(word[-1] in punctuation):
+		if(word[-1] in punctuation):	# words ending with punctuation
 			word = word[:-1]
-
 		caps.add(word)
-capsstart = set(re.findall('\.\s+([A-Za-z]+)', text))
-capsnotstart = caps - capsstart
 
+#capsstart = set(re.findall('\.\s+([A-Za-z]+)', text))
+#capsnotstart = caps - capsstart
 
+dictfile = open('/usr/share/dict/words', 'r')
+dictwords = dictfile.read().splitlines()
+
+x = set()
+for w in caps:
+	if (w.lower() in dictwords):
+		x.add(w)
+
+characters = caps - x
+
+x = set()
+for word in characters:
+	if (any([char in word for char in punctuation])):
+		x.add(word)
+
+characters = characters - x	
