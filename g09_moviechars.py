3af4c590605a1d84ec096eb4fedf1e7f6bc0e4e0
Pratyaksh Sharma <pratyaksh@nsl-117.cse.iitb.ac.in>
2014-03-07 16:55:52 +0530

    parsing caps

--- g09_moviechars.py
+++ g09_moviechars.py
@@ -1,8 +1,22 @@
+import re
+
 filename = input("Enter name of the text file: ")
 f = open(filename, 'r')
-data = set(f.read().split())
-allcaps = set()
-for word in data:
-	if (word.isupper()): 
-		allcaps.add(word)
+text = f.read()
+words = set(text.split())
+caps = set()
+
+punctuation = [',', '.', '\'', '\"', '!', '?', '...', ':']
+
+for word in words:
+	if (word[0].isupper()): 
+		if (word[-3:] in punctuation):
+			word = word[:-3]
+		if(word[-1] in punctuation):
+			word = word[:-1]
+
+		caps.add(word)
+capsstart = set(re.findall('\.\s+([A-Za-z]+)', text))
+capsnotstart = caps - capsstart
+
 
