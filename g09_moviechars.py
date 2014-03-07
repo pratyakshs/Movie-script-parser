0305293f671bebc05b56c93ebd03977ed7db8c22
Pratyaksh Sharma <pratyaksh@nsl-03.cse.iitb.ac.in>
2014-03-07 22:10:10 +0530

    Added count.py script

--- g09_moviechars.py
+++ g09_moviechars.py
@@ -1,4 +1,5 @@
 import re
+import operator
 
 filename = input("Enter name of the text file: ")
 f = open(filename, 'r')
@@ -31,7 +32,20 @@ characters = caps - x
 
 x = set()
 for word in characters:
-	if (any([char in word for char in punctuation])):
+	if (any([char in word for char in punctuation])): # remove words which have pos
 		x.add(word)
 
-characters = characters - x	
+characters = characters - x
+
+x = set()
+for word in characters:
+	if (not (word.isupper()) and (word.upper() in characters)): # remove multiple occurances of words in different case
+		x.add(word)
+characters = characters - x
+
+char_counts = dict()			   
+for word in characters:
+	char_counts[word] = text.count(word)
+	
+sorted_list = sorted(char_counts.items(), key = lambda char_counts: -char_counts[1])
+
